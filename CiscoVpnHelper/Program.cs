8e0a1dfab13d25be3705d119853e1a9abea0636c
TimAndy <xuchonglei@126.com>
2020-10-20 15:57:54 +0800

    只能单例运行

--- CiscoVpnHelper/Program.cs
+++ CiscoVpnHelper/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,10 +17,18 @@ namespace CiscoVpnHelper
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMain());
+            bool Run;
+            string Name = "4035a3d4-bdaa-4e18-b4ce-9ea2eef0b308";
+            Mutex Mutex = new Mutex(true, Name, out Run);
+            if (Run)
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new FrmMain());
+
+                Mutex.ReleaseMutex(); //添加此行,防止生成Release版本时Mutex无效.
+            }
         }
     }
 }
